fc1cd4ea43ec054890f217d858060dfed4afb0a9
maxdab34 <mdabout@henix.fr>
2023-09-04 15:37:44 -0400

    “security”

--- service/__init__.py
+++ service/__init__.py
@@ -4,14 +4,17 @@ Package for the application models and service routes
 This module creates and configures the Flask app and sets up the logging
 and SQL database
 """
+
 import sys
 from flask import Flask
 from service import config
 from service.common import log_handlers
+from flask_talisman import Talisman
 
 # Create Flask application
 app = Flask(__name__)
 app.config.from_object(config)
+talisman = Talisman(app)
 
 # Import the routes After the Flask app is created
 # pylint: disable=wrong-import-position, cyclic-import, wrong-import-order
